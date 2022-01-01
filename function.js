f2c59e0e8df5a18a24da6776e2f95a3e94d07283
notchioma <paullois829@gmail.com>
2022-01-01 09:59:10 +0100

    progress bar updated and book me linked to github

--- function.js
+++ function.js
@@ -16,14 +16,3 @@ var docWidth = document.documentElement.offsetWidth;
         }
     }
 );
-
-function bindEvent(e, eventName, callback) {
-    if (e.addEventListener) // new browsers
-        e.addEventListener(eventName, callback, false);
-    else if (e.attachEvent) // IE
-        e.attachEvent('on' + eventName, callback);
-};
-
-bindEvent(document.body, 'scroll', function (e) {
-    document.body.scrollLeft = 0;
-});
